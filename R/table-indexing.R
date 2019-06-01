e7d140ba966b2d947ec5860890834d47559da391
Bruce C. Schardt <schardt.bruce.curtis@gmail.com>
2019-06-01 15:56:57 -0700

    README additions

--- R/table-indexing.R
+++ R/table-indexing.R
@@ -13,7 +13,7 @@
 #' Default sql generation is appropriate for MariaDB and MySQL.
 #'
 #' @param con a DBConnection oject
-#' @param table a character string specifying the name of an existing DBMS table
+#' @param name a character string specifying the name of an existing DBMS table
 #' @param primary_key a character vector of column names which will form the primary key
 #'
 #'     To specify a composite Primary Key for the `symbol` and `date` columns use
@@ -26,18 +26,18 @@
 #' @family DBIConnection generics
 #' @export
 setGeneric("dbAddPrimaryKey",
-           def = function(con, table, primary_key, ...) standardGeneric("dbAddPrimaryKey")
+           def = function(con, name, primary_key, ...) standardGeneric("dbAddPrimaryKey")
 )
 
 #' @rdname hidden_aliases
 #' @export
 setMethod("dbAddPrimaryKey", signature("DBIConnection"),
-          function(con, table, primary_key, ...) {
-            stopifnot(dbExistsTable(con, table));
+          function(con, name, primary_key, ...) {
+            stopifnot(dbExistsTable(con, name));
             colsInTable <- DBI::dbListFields(con,table);
             pk <- as.character(unname(primary_key));
             stopifnot(setequal(pk,intersect(pk,colsInTable)));
-            query <- sqlAlterTableWithPrimaryKey(con, table, pk = primary_key, ...);
+            query <- sqlAlterTableWithPrimaryKey(con, name, pk = primary_key, ...);
             dbExecute(con,query);
           }
 )
@@ -56,7 +56,7 @@ setMethod("dbAddPrimaryKey", signature("DBIConnection"),
 #' Default sql generation is appropriate for MariaDB and MySQL.
 #'
 #' @param con a DBConnection oject
-#' @param table a character string specifying the name of an existing DBMS table
+#' @param name a character string specifying the name of an existing DBMS table
 #' @param index a character vector of column names which will form the index
 #' @param unique boolean, default = FALSE; If set to true, the constraint that all rows must have unique
 #'  values for the index
@@ -71,14 +71,14 @@ setMethod("dbAddPrimaryKey", signature("DBIConnection"),
 #' @family DBIConnection generics
 #' @export
 setGeneric("dbAddIndex",
-           def = function(con, table, index, unique = FALSE, ...) standardGeneric("dbAddIndex")
+           def = function(con, name, index, unique = FALSE, ...) standardGeneric("dbAddIndex")
 )
 
 #' @rdname hidden_aliases
 #' @export
 setMethod("dbAddIndex", signature("DBIConnection"),
-          function(con, table, index, unique = FALSE, ...) {
-            stopifnot(dbExistsTable(con, table));
+          function(con, name, index, unique = FALSE, ...) {
+            stopifnot(dbExistsTable(con, name));
             colsInTable <- DBI::dbListFields(con,table);
             idx <- as.character(unname(index));
             stopifnot(setequal(idx,intersect(idx,colsInTable)));
